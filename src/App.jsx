7589d81d016c816f41f0459f3b811275dfe744d9
SyabAhmad <syabblogger@gmail.com>
2026-05-02 22:50:47 +0300

    vercel.config added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Header from './components/Header'
 import Footer from './components/Footer'
+import ScrollToTop from './components/ScrollToTop'
 import Home from './pages/Home'
 import Menu from './pages/Menu'
 import About from './pages/About'
@@ -10,6 +11,7 @@ function Layout() {
   return (
     <div className="min-h-screen flex flex-col bg-white">
       <Header />
+      <ScrollToTop />
       <main className="flex-1 pt-16 sm:pt-20">
         <Routes>
           <Route path="/" element={<Home />} />
